d81ef77fa256a519520b79d272cbe2412ae34d73
Trilochan <trilochanprakash06@gmail.com>
2025-02-10 23:01:41 +0530

    doop

---
+++ components/footer.php
@@ -0,0 +1,35 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Us.</h3>
+         <a href="tel:9800000000"><i class="fas fa-phone"></i> +91 8778849959</a>
+         <a href="tel:9900000000"><i class="fas fa-phone"></i> +91 96123 15794</a>
+         <a href="mailto:harshchy143@gmail.com"><i class="fas fa-envelope"></i> WolfCart@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Chennai , Tamil Nadu </a>
+      </div>
+
+      
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Wolf Pack</span> | All rights reserved!</div>
+
+</footer>
